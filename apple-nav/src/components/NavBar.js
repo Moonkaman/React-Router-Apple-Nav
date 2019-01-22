4acbe888b3475122a5e0211ea838c2e4113ae3be
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-22 15:17:11 -0700

    Add minimal styles to nav items

--- apple-nav/src/components/NavBar.js
+++ apple-nav/src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 
+import "./Nav.css";
+
 const NavBar = props => {
   return (
     <nav>
