8520cd9dfdff9ae6da75b2d567ec9a5a902363b5
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-22 15:35:29 -0700

    Create a data file & setup first route

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
 import "./App.css";
 
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+
+import appleData from "./Data";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      navItems: ["Mac", "iPad", "iPhone", "Watch", "TV", "Music", "Support"]
+      navItems: ["Mac", "iPad", "iPhone", "Watch", "TV", "Music", "Support"],
+      appleNavData: null
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      appleNavData: appleData
+    });
   }
 
   render() {
     return (
       <div className="App">
         <NavBar navItems={this.state.navItems} />
+        <Route exact path="/" component={Home} />
       </div>
     );
   }
---
+++ apple-nav/src/Data.js
@@ -0,0 +1,222 @@
+const appleData = [
+  {
+    item: 'Mac',
+    subItems: [
+      {
+        name: 'MacBook',
+        img: null
+      },
+      {
+        name: 'MacBook Air',
+        img: null
+      },
+      {
+        name: 'MacBook Pro',
+        img: null
+      },
+      {
+        name: 'iMac',
+        img: null
+      },
+      {
+        name: 'iMac Pro',
+        img: null
+      },
+      {
+        name: 'Mac Pro',
+        img: null
+      },
+      {
+        name: 'Mac Mini',
+        img: null
+      },
+      {
+        name: 'Accessories',
+        img: null
+      },
+      {
+        name: 'Mojave',
+        img: null
+      },
+      {
+        name: 'Compare',
+        img: null
+      }
+    ]
+  },
+  {
+    item: 'iPad',
+    subItems: [
+      {
+        name: 'iPad Pro',
+        img: null
+      },
+      {
+        name: 'iPad Pro 10.5"',
+        img: null
+      },
+      {
+        name: 'iPad',
+        img: null
+      },
+      {
+        name: 'iPad mini 4',
+        img: null
+      },
+      {
+        name: 'iOS 12',
+        img: null
+      },
+      {
+        name: 'Apple Pencil',
+        img: null
+      },
+      {
+        name: 'Smart Keyboard',
+        img: null
+      },
+      {
+        name: 'Accessories',
+        img: null
+      },
+      {
+        name: 'Compare',
+        img: null
+      }
+    ]
+  },
+  {
+    item: 'iPhone',
+    subItems: [
+      {
+        name: 'iPhone Xs',
+        img: null
+      },
+      {
+        name: 'iPhone Xr',
+        img: null
+      },
+      {
+        name: 'iPhone 8',
+        img: null
+      },
+      {
+        name: 'iPhone 7',
+        img: null
+      },
+      {
+        name: 'iOS 12',
+        img: null
+      },
+      {
+        name: 'AirPods',
+        img: null
+      },
+      {
+        name: 'Accessories',
+        img: null
+      },
+      {
+        name: 'Compare',
+        img: null
+      }
+    ]
+  },
+  {
+    item: 'Watch',
+    subItems: [
+      {
+        name: 'Apple Watch Series 4',
+        img: null
+      },
+      {
+        name: 'Apple Watch Nike+',
+        img: null
+      },
+      {
+        name: 'Apple Watch Hermes',
+        img: null
+      },
+      {
+        name: 'Apple Watch Series 3',
+        img: null
+      },
+      {
+        name: 'watchOS',
+        img: null
+      },
+      {
+        name: 'Bands',
+        img: null
+      },
+      {
+        name: 'Accessories',
+        img: null
+      },
+      {
+        name: 'Compare',
+        img: null
+      }
+    ]
+  },
+  {
+    item: 'TV',
+    subItems: [
+      {
+        name: 'Apple TV 4K',
+        img: null
+      },
+      {
+        name: 'Apple TV',
+        img: null
+      },
+      {
+        name: 'TV App',
+        img: null
+      },
+      {
+        name: 'Accessories',
+        img: null
+      },
+      {
+        name: 'Compare',
+        img: null
+      }
+    ]
+  },
+  {
+    item: 'Music',
+    subItems: [
+      {
+        name: 'Apple Music',
+        img: null
+      },
+      {
+        name: 'iTunes',
+        img: null
+      },
+      {
+        name: 'HomePod',
+        img: null
+      },
+      {
+        name: 'AirPods',
+        img: null
+      },
+      {
+        name: 'iPod touch',
+        img: null
+      },
+      {
+        name: 'Music Accessories',
+        img: null
+      },
+      {
+        name: 'Gift Cards',
+        img: null
+      }
+    ]
+  }
+]
+
+export default appleData;
